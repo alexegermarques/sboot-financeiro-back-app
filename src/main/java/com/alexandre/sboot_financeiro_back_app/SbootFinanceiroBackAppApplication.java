43ed91d3c6dce64de5b582b20002efbeca39273e
Alexandre Marques <alexandre.marques@portoseguro.com.br>
2024-06-18 12:40:44 -0300

    Primeiro commit

---
+++ src/main/java/com/alexandre/sboot_financeiro_back_app/SbootFinanceiroBackAppApplication.java
@@ -0,0 +1,12 @@
+package com.alexandre.sboot_financeiro_back_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootFinanceiroBackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootFinanceiroBackAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexandre/sboot_financeiro_back_app/SbootFinanceiroBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.sboot_financeiro_back_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootFinanceiroBackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
